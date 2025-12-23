1fce583031ccb664359602acc343e4d6061df325
Carlin233 <carlin233@gmail.com>
2025-12-22 22:59:15 -0300

    RESOLVENDO BUG

--- backend/db.js
+++ backend/db.js
@@ -1,26 +1,18 @@
 const sqlite3 = require("sqlite3").verbose()
 
-const db = new sqlite3.Database("./amigo_secreto.db")
+const db = new sqlite3.Database(
+  process.env.DB_PATH || "./amigo_secreto.db"
+)
 
 db.serialize(() => {
   db.run(`
     CREATE TABLE IF NOT EXISTS participantes (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
-      nome TEXT NOT NULL,
+      nome TEXT NOT NULL UNIQUE,
       sorteado TEXT
     )
   `)
 })
-
-const participantesFixos = [
-  "Sharlise",
-  "Gabrielle",
-  "Ledinha",
-  "Adriana",
-  "Aline",
-  "Jovina",
-  "Ana Clara"
-]
 
 participantesFixos.forEach(nome => {
   db.run(
@@ -28,6 +20,5 @@ participantesFixos.forEach(nome => {
     [nome]
   )
 })
-
 
 module.exports = db
